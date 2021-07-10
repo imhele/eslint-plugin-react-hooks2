5390743b3f01609d5e99ce343c6af650f40dbd28
imhele <work@imhele.com>
2021-07-10 15:52:30 +0800

    chore: init repo

---
+++ wc-bundler.config.ts
@@ -0,0 +1,13 @@
+import type { CoreConfig } from 'wc-bundler';
+
+const Config: CoreConfig = {
+  babel: {
+    env: {
+      targets: {
+        node: '>=10',
+      },
+    },
+  },
+};
+
+export default Config;
