80f1a2324ba94c12567a810b0e9e332d8e47c770
imhele <work@imhele.com>
2021-07-11 00:23:21 +0800

    fix: rename plugin

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ import ExhaustiveDeps from './ExhaustiveDeps';
 
 export const configs = {
   recommended: {
-    plugins: ['react-hooks'],
+    plugins: ['react-hooks2'],
     rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks2/rules-of-hooks': 'error',
+      'react-hooks2/exhaustive-deps': 'warn',
     },
   },
 };
